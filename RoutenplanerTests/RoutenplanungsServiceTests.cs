528d35233a212a844f657b0204c3034572c0a7d9
Viktor <vjarai@users.noreply.github.com>
2025-09-05 12:53:14 +0200

    .

--- RoutenplanerTests/RoutenplanungsServiceTests.cs
+++ RoutenplanerTests/RoutenplanungsServiceTests.cs
@@ -1,19 +1,67 @@
 ﻿using Xunit;
 using Routenplaner;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Moq;
 
 namespace Routenplaner.Tests
 {
     public class RoutenplanungsServiceTests
     {
-        [Fact()]
-        public void TestTest()
+        [Fact]
+        public void PlaneRoute_DelegatesToStrategy_ReturnsExpectedResult()
         {
-            Xunit.Assert.Fail("This test needs an implementation");
+            // Arrange
+            var mockStrategie = new Mock<IRoutenStrategie>();
+            mockStrategie.Setup(s => s.PlaneRoute("A", "B")).Returns("Dummy: A -> B");
+            var service = new RoutenplanungsService(mockStrategie.Object);
+
+            // Act
+            var result = service.PlaneRoute("A", "B");
+
+            // Assert
+            Assert.Equal("Dummy: A -> B", result);
+            mockStrategie.Verify(s => s.PlaneRoute("A", "B"), Times.Once);
+        }
+
+        [Fact]
+        public void PlaneRoute_WithAutoRoutenStrategie_ReturnsAutoRoute()
+        {
+            // Arrange
+            var strategie = new AutoRoutenStrategie();
+            var service = new RoutenplanungsService(strategie);
+
+            // Act
+            var result = service.PlaneRoute("Berlin", "Hamburg");
+
+            // Assert
+            Assert.Equal("Route von Berlin nach Hamburg mit dem Auto geplant.", result);
+        }
+
+        [Fact]
+        public void PlaneRoute_WithFahrradRoutenStrategie_ReturnsFahrradRoute()
+        {
+            // Arrange
+            var strategie = new FahrradRoutenStrategie();
+            var service = new RoutenplanungsService(strategie);
+
+            // Act
+            var result = service.PlaneRoute("München", "Augsburg");
+
+            // Assert
+            Assert.Equal("Route von München nach Augsburg mit dem Fahrrad geplant.", result);
+        }
+
+        [Fact]
+        public void PlaneRoute_WithBahnRoutenStrategie_ReturnsBahnRoute()
+        {
+            // Arrange
+            var strategie = new BahnRoutenStrategie();
+            var service = new RoutenplanungsService(strategie);
+
+            // Act
+            var result = service.PlaneRoute("Köln", "Düsseldorf");
+
+            // Assert
+            Assert.Equal("Route von Köln nach Düsseldorf mit der Bahn geplant.", result);
         }
     }
 }
